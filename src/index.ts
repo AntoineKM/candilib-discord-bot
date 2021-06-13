bcb02b153e457d1e37afc8798d77dd9615a745d5
Antoine Kingue <etorefr@gmail.com>
2021-06-14 00:08:16 +0200

    ✨ add departments to reminder

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import { CronJob } from "cron";
 import { Client, TextChannel } from "discord.js";
 import "dotenv-safe/config";
 
+import departmentsData from "./data/departements.json";
+
 const client = new Client();
-const token = process.env.TOKEN;
-const channelId = process.env.CHANNEL_ID;
-const roleSansPermisId = process.env.ROLE_SANS_PERMIS_ID;
+const token = process.env.TOKEN || "";
+const channelId = process.env.CHANNEL_ID || "";
+const roleSansPermisId = process.env.ROLE_SANS_PERMIS_ID || "";
 
 const runReminder = () => {
   const channel = client.channels.cache.get(channelId);
-  if (channel.isText) {
-    (<TextChannel>channel).send(`:alarm_clock: **Rappel pour les <@&${roleSansPermisId}>**\n`)
+  if (channel && channel.isText) {
+    (<TextChannel>channel).send(`:alarm_clock: **Rappel du jour pour les <@&${roleSansPermisId}>**\n${departmentsData.map(departement => `:round_pushpin: ${departement}: <https://beta.interieur.gouv.fr/candilib/candidat/${departement}/selection/selection-centre>`).join("\n")}`)
   }
 }
 
@@ -18,7 +20,7 @@ const main = () => {
   let reminderTask = new CronJob('00 00 12 * * *', runReminder);
 
   client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    // console.log(`Logged in as ${client.user.tag}!`);
 
     reminderTask.start();
     runReminder();
